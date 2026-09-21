5f6f7409eed7def01cc7613204636b2de5860de9
Mariam Omara <mariamomara2005@gmail.com>
2026-09-22 01:28:57 +0300

    Q2 PRACT

--- OOPASS-04/Shipment.cs
+++ OOPASS-04/Shipment.cs
@@ -8,3 +8,19 @@
 //    public decimal DeliveryFee { get; set; }
 //    public DeliveryAddress Destination { get; set; }
 //}
+
+
+
+//Q2 PRACT
+//public abstract class Shipment
+//{
+//    public string TrackingCode { get; set; }
+//    public string Description { get; set; }
+//    public decimal Weight { get; set; }
+//    public decimal DeliveryFee { get; set; }
+//    public DeliveryAddress Destination { get; set; }
+
+//    public abstract decimal EstimatedCost { get; }
+
+//    public abstract void PrintShipment();
+//}
