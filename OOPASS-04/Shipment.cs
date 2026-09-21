11d90ec8efdc6940285e7dde21e742121eb8ffd4
Mariam Omara <mariamomara2005@gmail.com>
2026-09-22 02:23:28 +0300

    Q7 P

--- OOPASS-04/Shipment.cs
+++ OOPASS-04/Shipment.cs
@@ -208,31 +208,31 @@
 
 
 
+//Q7 PR
 
+//public abstract class Shipment
+//{
+//    public string TrackingCode { get; set; }
+//    public string Description { get; set; }
+//    public decimal Weight { get; set; }
+//    public decimal DeliveryFee { get; set; }
+//    public DeliveryAddress Destination { get; set; }
 
-public abstract class Shipment
-{
-    public string TrackingCode { get; set; }
-    public string Description { get; set; }
-    public decimal Weight { get; set; }
-    public decimal DeliveryFee { get; set; }
-    public DeliveryAddress Destination { get; set; }
+//    public Shipment(
+//        string trackingCode,
+//        string description,
+//        decimal weight,
+//        decimal deliveryFee,
+//        DeliveryAddress destination)
+//    {
+//        TrackingCode = trackingCode;
+//        Description = description;
+//        Weight = weight;
+//        DeliveryFee = deliveryFee;
+//        Destination = destination;
+//    }
 
-    public Shipment(
-        string trackingCode,
-        string description,
-        decimal weight,
-        decimal deliveryFee,
-        DeliveryAddress destination)
-    {
-        TrackingCode = trackingCode;
-        Description = description;
-        Weight = weight;
-        DeliveryFee = deliveryFee;
-        Destination = destination;
-    }
+//    public abstract decimal EstimatedCost { get; }
 
-    public abstract decimal EstimatedCost { get; }
-
-    public abstract void PrintShipment();
-}
+//    public abstract void PrintShipment();
+//}
