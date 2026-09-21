49bbb2b20b976bc7e4d1751e5a6eff59f07a9034
Mariam Omara <mariamomara2005@gmail.com>
2026-09-22 01:23:42 +0300

    النظريQ1

--- OOPASS-04/Program.cs
+++ OOPASS-04/Program.cs
@@ -8,3 +8,19 @@
         }
     }
 }
+
+
+//النظريQ1
+
+//a/ What is Abstraction in Object-Oriented Programming?
+
+//Abstraction is the process of hiding unnecessary implementation details and showing only the essential features of an object.
+
+//B
+//Why is abstraction considered one of the four pillars of OOP?
+
+//Abstraction is one of the four pillars of OOP because it reduces complexity by hiding unnecessary details and exposing only the essential functionality.
+
+
+
+
