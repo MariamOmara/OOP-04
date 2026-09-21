d5295589d6cd4df57571da3f95eb316e1b8a231d
Mariam Omara <mariamomara2005@gmail.com>
2026-09-22 02:22:55 +0300

    Q7

---
+++ OOPASS-04/DeliveryCenter.cs
@@ -0,0 +1,22 @@
+﻿////Q7
+
+//using System;
+//using System.Collections.Generic;
+
+//public class DeliveryCenter
+//{
+//    private List<Shipment> shipments = new List<Shipment>();
+
+//    public void AddShipment(Shipment shipment)
+//    {
+//        shipments.Add(shipment);
+//    }
+
+//    public void PrintTrackingStatuses()
+//    {
+//        foreach (ITrackable shipment in shipments)
+//        {
+//            Console.WriteLine(shipment.GetTrackingStatus());
+//        }
+//    }
+//}
--- OOPASS-04/Shipment.cs
+++ OOPASS-04/Shipment.cs
@@ -203,3 +203,36 @@
 //        return EstimatedCost * 0.12m;
 //    }
 //}
+
+
+
+
+
+
+
+public abstract class Shipment
+{
+    public string TrackingCode { get; set; }
+    public string Description { get; set; }
+    public decimal Weight { get; set; }
+    public decimal DeliveryFee { get; set; }
+    public DeliveryAddress Destination { get; set; }
+
+    public Shipment(
+        string trackingCode,
+        string description,
+        decimal weight,
+        decimal deliveryFee,
+        DeliveryAddress destination)
+    {
+        TrackingCode = trackingCode;
+        Description = description;
+        Weight = weight;
+        DeliveryFee = deliveryFee;
+        Destination = destination;
+    }
+
+    public abstract decimal EstimatedCost { get; }
+
+    public abstract void PrintShipment();
+}
